13c92cf44f47623ef9fa6c4163e851f13e5d8972
William Panting <willtp87@hotmail.com>
2012-08-21 14:27:29 -0300

    attributes now showing up and better nesting of elements

--- islandora_XML_to_list.tpl.php
+++ islandora_XML_to_list.tpl.php
@@ -17,37 +17,40 @@
    *   An XML element to render in a list
    */
   function print_XML_in_list($XML_element) {
-    print('<UL>');
-    $element_attributes = array();
-    $element_text = '';
 
     // Text node print.
     print ('<LI>');
-      print ($XML_element->tagName . ':  ' . $XML_element->textContent);
-    print ('</LI>');
+    print ($XML_element->tagName . ':  ' . $XML_element->nodeValue);
+
 
     // Print attributes.
-    if ($element_attributes) :
-      foreach ($element_attributes as $attribute) :
+    if (!empty($XML_element->attributes)):
+      print('<UL>');
+      foreach ($XML_element->attributes as $attribute) :
         print ('<LI>');
-          print ('<STRONG>' . $attribute_name . '</STRONG> :  ' . $value);
+        print ('<STRONG>' . $attribute->name . '</STRONG> :  ' . $attribute->value);
         print ('</LI>');
       endforeach;
+      print ('</UL>');
     endif;
-
     // If the current element has children send them through the print process.
     if($XML_element->hasChildNodes()) :
       foreach($XML_element->childNodes as $child_element) :
-        print_XML_in_list($child_element);
+        if ($child_element->nodeType != XML_TEXT_NODE):
+          print_XML_in_list($child_element);
+        endif;
       endforeach;
     endif;
 
-    print('</UL>');
+    print ('</LI>');
   }
 
   // Get root document and call into the recursive function.
   $datastream_DOM = DOMDocument::loadXML($XML_datastream);
   $XML_root_element = $datastream_DOM->documentElement;
+
+  print('<UL>');
   print_XML_in_list($XML_root_element);
+  print('</UL>');
 ?>
 </div>
