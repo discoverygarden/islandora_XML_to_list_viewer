efa6d0ae8b98d03e24e125b8eb6d3efc751d4192
William Panting <willtp87@hotmail.com>
2012-09-26 11:59:36 -0300

    fixed including function more than once error, refactored out islandora fedora api

--- islandora_XML_to_list.tpl.php
+++ islandora_XML_to_list.tpl.php
@@ -9,41 +9,43 @@
 ?>
 <div class="islandora_XML_to_list_viewer">
 <?php
-  /**
-   * recursive function that prints an
-   * XML element to an unordered list
-   *
-   * @param mixed $XML_element
-   *   An XML element to render in a list
-   */
-  function print_XML_in_list($XML_element) {
+  if(!function_exists('print_XML_in_list')):
+    /**
+     * recursive function that prints an
+     * XML element to an unordered list
+     *
+     * @param mixed $XML_element
+     *   An XML element to render in a list
+     */
+    function print_XML_in_list($XML_element) {
 
-    // Text node print.
-    print ('<LI>');
-    print ($XML_element->tagName . ':  ' . $XML_element->nodeValue);
+      // Text node print.
+      print ('<LI>');
+      print ($XML_element->tagName . ':  ' . $XML_element->nodeValue);
 
 
-    // Print attributes.
-    if (!empty($XML_element->attributes)):
-      print('<UL>');
-      foreach ($XML_element->attributes as $attribute) :
-        print ('<LI>');
-        print ('<STRONG>' . $attribute->name . '</STRONG> :  ' . $attribute->value);
-        print ('</LI>');
-      endforeach;
-      print ('</UL>');
-    endif;
-    // If the current element has children send them through the print process.
-    if($XML_element->hasChildNodes()) :
-      foreach($XML_element->childNodes as $child_element) :
-        if ($child_element->nodeType != XML_TEXT_NODE):
-          print_XML_in_list($child_element);
-        endif;
-      endforeach;
-    endif;
+      // Print attributes.
+      if (!empty($XML_element->attributes)):
+        print('<UL>');
+        foreach ($XML_element->attributes as $attribute):
+          print ('<LI>');
+          print ('<STRONG>' . $attribute->name . '</STRONG> :  ' . $attribute->value);
+          print ('</LI>');
+        endforeach;
+        print ('</UL>');
+      endif;
+      // If the current element has children send them through the print process.
+      if($XML_element->hasChildNodes()) :
+        foreach($XML_element->childNodes as $child_element) :
+          if ($child_element->nodeType != XML_TEXT_NODE):
+            print_XML_in_list($child_element);
+          endif;
+        endforeach;
+      endif;
 
-    print ('</LI>');
-  }
+      print ('</LI>');
+    }
+  endif;
 
   // Get root document and call into the recursive function.
   $datastream_DOM = DOMDocument::loadXML($XML_datastream);
